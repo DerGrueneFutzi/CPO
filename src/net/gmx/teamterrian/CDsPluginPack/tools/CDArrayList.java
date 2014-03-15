6348670be173bcd8d1376bd92231d5f6520018ff
GruenerFutzi <jaegermax1997@web.de>
2014-03-15 17:24:08 +0100

    Implemente own ArrayList class

---
+++ src/net/gmx/teamterrian/CDsPluginPack/tools/CDArrayList.java
@@ -0,0 +1,62 @@
+package net.gmx.teamterrian.CDsPluginPack.tools;
+
+import java.util.ArrayList;
+
+import org.bukkit.entity.Player;
+
+public class CDArrayList<E> extends ArrayList<E>
+{
+	private static final long serialVersionUID = -1198161659015956750L;
+	
+	public boolean contains(Object o)
+	{
+		if(!(o instanceof Player)) return super.contains(o);
+		Player p, po = (Player) o;
+		for(Object val : this)
+		{
+			if(!(val instanceof Player)) return super.contains(o);
+			p = (Player) val;
+			if(p.getName().equals(po.getName())) return true;
+		}
+		return false;
+	}
+	
+	public int indexOf(Object o)
+	{
+		if(!(o instanceof Player)) return super.indexOf(o);
+		Player p, po = (Player) o;
+		for(int i = 0; i < this.size(); i++)
+		{
+			p = (Player) this.get(i);
+			if(p.getName().equals(po.getName())) return i;
+		}
+		return -1;
+	}
+	public int lastIndexOf(Object o)
+	{
+		if(!(o instanceof Player)) return super.lastIndexOf(o);
+		Player p, po = (Player) o;
+		for(int i = this.size() - 1; i >= 0; i--)
+		{
+			p = (Player) this.get(i);
+			if(p.getName().equals(po.getName())) return i;
+		}
+		return -1;
+	}
+	
+	public boolean remove(Object o)
+	{
+		if(!(o instanceof Player)) return super.remove(o);
+		Player p, po = (Player) o;
+		for(int i = 0; i < this.size(); i++)
+		{
+			p = (Player) this.get(i);
+			if(p.getName().equals(po.getName()))
+			{
+				this.remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
+}
